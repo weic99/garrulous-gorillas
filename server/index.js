26304d75aea56f14a8fcb69505c2887e453f0f65
Wei Chen <tengwei1@gmail.com>
2017-07-31 19:40:25 -0400

    added passport, model-user,

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const passport = require('passport');
+
+// Connect to mongoDB
+const config = require('./config/mongo');
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database', config.database);
+});
+
+// Server setup
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+// Routes
+app.get('*', (req, res) => {
+  res.send('hello');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log('Server started on port ', port);
+});
