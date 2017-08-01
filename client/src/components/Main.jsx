e9c39b713c866782ba214fe4e88769aec99c4640
Angela Lin <tigclaw@gmail.com>
2017-08-01 14:17:22 -0400

    Add basic jsx files for Nav and Main views

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+			<div>
+			List of current debates goes here
+			<li>Topic #1</li>
+			<li>Topic #2</li>
+			</div>
+		)
+	}
+
+}
+
+export default Main;
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Nav extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+			<div>
+			<li>Login/Signup</li>
+			<li>Current Debates</li>
+			</div>
+		)
+	}
+}
+
+export default Nav;
