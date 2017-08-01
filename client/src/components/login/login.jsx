94adb2b580ac83ce306405530c0721e640e88a6c
Wei Chen <tengwei1@gmail.com>
2017-08-01 19:05:13 -0400

    modal login

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, ControlLabel, Form, FormControl, FormGroup } from 'react-bootstrap';
+const Modal = require('react-bootstrap-modal');
 
 class Login extends React.Component {
   constructor() {
@@ -28,20 +29,30 @@ class Login extends React.Component {
     
     return (
       <div>
-        <Form inline>
-            <FormGroup controlId="formHorizontalEmail">
-                <ControlLabel>username </ControlLabel>
-                <FormControl type="username" ref="username" onChange={this.handleChange} placeholder="username" />
-            </FormGroup>
-            <FormGroup controlId="formHorizontalPassword">
-                <ControlLabel>Password </ControlLabel>
-                <FormControl type="password" ref="password" onChange={this.handleChange} placeholder="Password" />
-            </FormGroup>
-            <Button onClick={(event) => this.handleSubmit(event)}>Login</Button>
-            {errorMessage &&
-            <p style={{color:'red'}}>{errorMessage}</p>
-            }
-        </Form>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form inline>
+                <FormGroup controlId="formHorizontalEmail">
+                    <ControlLabel>username </ControlLabel>
+                    <FormControl type="username" ref="username" onChange={this.handleChange} placeholder="username" />
+                </FormGroup>
+                <FormGroup controlId="formHorizontalPassword">
+                    <ControlLabel>Password </ControlLabel>
+                    <FormControl type="password" ref="password" onChange={this.handleChange} placeholder="Password" />
+                </FormGroup>
+                <Button onClick={(event) => this.handleSubmit(event)}>Login</Button>
+                {errorMessage &&
+                <p style={{color:'red'}}>{errorMessage}</p>
+                }
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     )
   }
