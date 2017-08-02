4acc1f011c7c21134ef93df17123f29e8c288f33
Brenden Williams <brendenw7@gmail.com>
2017-08-02 17:03:59 -0400

    CSS stuff

--- client/src/components/debate/DebateFloor.jsx
+++ client/src/components/debate/DebateFloor.jsx
@@ -6,31 +6,53 @@ class DebateFloor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      argumentsPro: ['These are my feeeeeeeeeelings', 'Here are moreeeee feeeeelings', 'I LOVEEEEEE IT'],
-      argumentsCon: ['You are wrong', 'OBJECTTTTIONNNNNNNNN', 'I HATEEEEEEEE THATTTTTT'],
-    }
+      // These should be populated by the DB, or sockets for each specific debate
+      argumentsFor: ['These are my feeeeeeeeeelings', 'Here are moreeeee feeeeelings', 'I LOVEEEEEE IT', 'Clearly you are mistake. I CANNOT BELIEVE THIS'],
+      argumentsAgainst: ['You are wrong', 'OBJECTTTTIONNNNNNNNN', 'I HATEEEEEEEE THATTTTTT', 'We do not believe in such savage ideas'],
+      votesFor: 5,
+      votesAgainst: 11,
+      winner: "",
+      positions: [
+        {
+          position: 'For',
+          points: 0,
+          arguments: ['These are my feeeeeeeeeelings', 'Here are moreeeee feeeeelings', 'I LOVEEEEEE IT', 'Clearly you are mistake. I CANNOT BELIEVE THIS'],
 
+        },
+        {
+          position: 'Against',
+          points: 0,
+          arguments: ['You are wrong', 'OBJECTTTTIONNNNNNNNN', 'I HATEEEEEEEE THATTTTTT', 'We do not believe in such savage ideas'],
+        },
+      ]    
+    }
+    this.handleVote = this.handleVote.bind(this);
   }
 
+  handleVote(event) {
+    // When a box is checked, update the value of values by 1, to the proper debate ID and proper position
+    console.log('clicked');
+
+  }
   componentWillMount() {
 
     
   }
-
   componentDidMount() {
-    // Query Database
+    // Query Database using set interval and axios for the amount of votes, and arguments
 
   }
 
   render() {
     return (
-      <div>
-        <div>Topic</div>
-        <Position position="Pro" arguments={this.state.argumentsPro} points="10" />
-        <Position position="Con" arguments={this.state.argumentsCon} points="7" />
+      <div className="container">
+        <div className="Row">
+          <div>Topic</div>
+          {this.state.positions.map((position, index) => <Position handleVote={this.handleVote} key={index} position={position} />)}
+  
+        </div>
       </div>
       )
   }
 }
   export default DebateFloor;
-
--- client/src/components/debate/Position.jsx
+++ client/src/components/debate/Position.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Argument from './Argument.jsx'
+const socket = require('socket.io-client')('http://localhost:3000');
 
-const Position = (props) => (
-  <div>
-    <h4>{props.position}</h4>
-    <div>{props.points} Points</div>
-    {props.arguments.map( (argument, i) => <Argument argument={argument} key={i}/>)}
-  </div>
-  )
+class Position extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+    socket.on('chat', (data) => {
+      const output = ReactDOM.findDOMNode(this.refs.output);
+      output.innerHTML += `<p><strong>${data.username}:</strong>${data.message}</p>`;
+      output.lastChild.scrollIntoView();
+    });
+  }
+
+  
+
+
+  render() {
+    return (
+      <div className='col-sm-6'>
+        <h4>{this.props.position.position}</h4>
+        <div>{this.props.position.points} Points</div>
+        {this.props.position.arguments.map( (argument, i) => <Argument handleVote={this.props.handleVote} argument={argument} key={i}/>)}
+      </div>
+    )
+  }
+}
 
 export default Position;
