473ee13879f4d01b9323dda111e547c071c7d0da
Angela Lin <tigclaw@gmail.com>
2017-08-02 11:11:27 -0400

    Add debate foreign key in db schemas (chat, activeDebate)

--- server/models/activeDebate.js
+++ server/models/activeDebate.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const ActiveDebateSchema = mongoose.Schema({
+	debate: { type: Schema.Types.ObjectId, ref: 'Debate' },
   for: { type: Schema.Types.ObjectId, ref: 'For' },
   against: { type: Schema.Types.ObjectId, ref: 'Against' },
   spectator: { type: Schema.Types.ObjectId, ref: 'Spectator' }
